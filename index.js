ecce4b20fd49744c39dec4e7785485a5d0ad9c37
Anfer Valera <anfervalera11@gmail.com>
2021-08-12 23:40:43 -0400

    Primera parte movil completada

---
+++ index.js
@@ -0,0 +1,14 @@
+const buscarCiudad = document.querySelector("#buscar-ciudad")
+const cerrar = document.querySelector("#cerrar")
+
+buscarCiudad.addEventListener("click", function(ev){
+    const menu = document.getElementById("menu-lateral")
+    menu.classList.add("aparecer")
+    menu.classList.remove("hide")
+})
+
+cerrar.addEventListener("click", function(ev){
+    const menu = document.getElementById("menu-lateral")
+    menu.classList.remove("aparecer")
+    menu.classList.add("hide")
+})
